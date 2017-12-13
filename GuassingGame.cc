79ad75cf389fe5e839725f856c821645f3ff17eb
Kirubel Tadesse <kirubel.m.tadesse@gmail.com>
2017-12-12 22:42:43 -0500

    gueassinggame

---
+++ GuassingGame.cc
@@ -0,0 +1,40 @@
+//include random number library
+#include<iostream>
+
+#incldue<random>
+using namespace std;
+
+
+//function for the game
+
+	//generate and store the variable
+
+	//get the input from the user
+
+	//compare the input with the generated number 
+
+	//if the guessed number is to high 
+
+		//print out and the guessed number is too high and ask if they want to play again
+
+		//if yes 
+			
+			//loop again
+
+		//esle 
+			
+			//thank you and exist
+
+
+int main(){
+//ask user if they want to play
+
+//if yes 
+
+	//go to game
+
+//else
+	//next time
+
+
+}
